d7eea10796937b83253c490af057c3d5e8641d23
Joshh742 <revildobusdan@gmail.com>
2025-10-23 03:04:14 +0800

    Tambah Foto Profil

--- index.php
+++ index.php
@@ -14,5 +14,7 @@ require_once '../app/config/config.php';
     <p>I'm Andreas from Manado.</p>
     <p>I'm 20 years old this year.</p>
     <p>I'm Mahasiswa Unklab.</p>
+
+    <img src="images/opah.jpg" alt="Foto profil Andreas" width="300">
 </body>
 </html>
