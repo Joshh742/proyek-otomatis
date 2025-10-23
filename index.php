79cc991e474632f89efa101271d19ddf3f8d3b3c
Joshh742 <revildobusdan@gmail.com>
2025-10-23 16:17:24 +0800

    Ganti Profesi

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ require_once '../app/config/config.php';
     <h1>This is my Home Page.</h1>
     <p>I'm Andreas from Manado.</p>
     <p>I'm 20 years old this year.</p>
-    <p>I'm Mahasiswa Unklab.</p>
+    <p>I'm Dosen Unklab.</p>
 
     <img src="images/opah.jpg" alt="Foto profil Andreas" width="300">
 </body>
